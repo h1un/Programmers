6ff1cf57e006559aad4bfdde91d37155cc7fb9ee
h1un <h1unha@naver.com>
2022-01-22 03:17:38 +0900

    feat : x만큼 간격이 있는 n개의 숫자

--- src/test/java/level1/x만큼_간격이_있는_n개의_숫자/SolutionTest.java
+++ src/test/java/level1/x만큼_간격이_있는_n개의_숫자/SolutionTest.java
@@ -1,23 +1,19 @@
 package level1.x만큼_간격이_있는_n개의_숫자;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SolutionTest {
+
+    Solution solution = new Solution();
 
     @Test
     public void solution() {
         int x = 2;
         int n = 6;
-        long[] test;
         long[] answer = {2, 4, 6, 8, 10, 12};
+        long[] test = solution.solution(x, n);
 
-        Solution sol = new Solution();
-        test = sol.solution(x, n);
-
-        Assert.assertThat(answer, CoreMatchers.is(test));
+        Assert.assertArrayEquals(answer, test);
     }
 }
