8a2d834a9552ce9c53d02b31264853ab1d9cdcb0
h1un <h1unha@naver.com>
2022-01-24 03:51:49 +0900

    refactor : 문자열 내 p와 y의 개수 리팩토링

--- src/main/java/level1/문자열_내_p와_y의_개수/Solution.java
+++ src/main/java/level1/문자열_내_p와_y의_개수/Solution.java
@@ -2,12 +2,10 @@ package level1.문자열_내_p와_y의_개수;
 
 public class Solution {
     boolean solution(String s) {
-        int total = 0;
-        for(int i=0; i<s.length(); i++) {
-            if(s.substring(i, i+1).equalsIgnoreCase("p")) total++;
-            else if(s.substring(i, i+1).equalsIgnoreCase("y")) total--;
-        }
 
-        return total == 0 ? true : false;
+        s = s.toUpperCase();
+
+        return s.chars().filter(c -> c == 'P').count() == s.chars().filter(c -> c == 'Y').count();
     }
 }
+
--- src/test/java/level1/문자열_내_p와_y의_개수/SolutionTest.java
+++ src/test/java/level1/문자열_내_p와_y의_개수/SolutionTest.java
@@ -1,19 +1,16 @@
 package level1.문자열_내_p와_y의_개수;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.Assert.*;
-
-public class SolutionTest {
+class SolutionTest {
 
     Solution solution = new Solution();
-    @Test
-    public void solution() {
 
-        boolean b = solution.solution("pPoooyY");
-
-        Assert.assertTrue(b);
-
+    @ParameterizedTest
+    @CsvSource({"pPoooyY, true", "Pyy, false"})
+    public void solution(String s, boolean b) {
+        Assert.assertSame(solution.solution(s), b);
     }
 }
