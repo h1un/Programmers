df7f8f1e19d05d625095a438add532b133789a2d
h1un <h1unha@naver.com>
2022-02-23 02:13:27 +0900

    feat : 정렬 k번째수

---
+++ src/main/java/정렬/K번째수/Solution.java
@@ -0,0 +1,18 @@
+package 정렬.K번째수;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+
+            int[] array_ = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(array_);
+            answer[i] = array_[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/정렬/K번째수/SolutionTest.java
@@ -0,0 +1,17 @@
+package 정렬.K번째수;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    Solution solution = new Solution();
+
+    @Test
+    public void solution() {
+
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        Assert.assertArrayEquals(new int[]{5, 6, 3}, solution.solution(array, commands));
+
+    }
+}
