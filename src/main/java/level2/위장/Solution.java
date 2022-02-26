8ffd335f006927ab53eda7e2469ef8e64b896b6d
h1un <h1unha@naver.com>
2022-02-26 18:01:44 +0900

    feat : 위장

--- src/main/java/level2/위장/Solution.java
+++ src/main/java/level2/위장/Solution.java
@@ -6,19 +6,23 @@ public class Solution {
 
     public int solution(String[][] clothes) {
 
-        HashMap<String, String> clothesMap = new HashMap<>();
+
+        //단순 조합 개수만 구하는거기 때문에 개수만 저장
+        // 조합 수  = (의상개수 + 1(착용하지 않음)) * --- * (의상 개수 +1) - (아무것도 착용하지 않은상태(최소 한개의 의상을 입어여한다.))
+        HashMap<String, Integer> clothesMap = new HashMap<>();
 
         for (String[] clothesSet : clothes) {
-            //키 , 값
-            clothesMap.put(clothesSet[0], clothesSet[1]);
-
+            clothesMap.put(clothesSet[1], clothesMap.getOrDefault(clothesSet[1], 0) + 1);
         }
 
-        clothesMap.forEach((s, s2) -> System.out.println(s + "   " + s2));
+        int answer = 1;
 
-        int answer = 0;
+        for (String clothesType : clothesMap.keySet()) {
+            answer *= (clothesMap.get(clothesType) + 1);
+        }
 
-        return answer;
+
+        return answer - 1;
     }
 
 }
