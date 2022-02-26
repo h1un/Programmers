aeef94419bd6515d6f3046a96ce8fec1d57ac92f
h1un <h1unha@naver.com>
2022-02-26 18:06:27 +0900

    refactor : 위장 _ 다른사람의 풀이 공부하기

--- src/main/java/level2/위장/Solution.java
+++ src/main/java/level2/위장/Solution.java
@@ -1,11 +1,15 @@
 package level2.위장;
 
+import java.util.Arrays;
 import java.util.HashMap;
+
+import static java.util.stream.Collectors.*;
 
 public class Solution {
 
     public int solution(String[][] clothes) {
 
+        /*
 
         //단순 조합 개수만 구하는거기 때문에 개수만 저장
         // 조합 수  = (의상개수 + 1(착용하지 않음)) * --- * (의상 개수 +1) - (아무것도 착용하지 않은상태(최소 한개의 의상을 입어여한다.))
@@ -23,6 +27,15 @@ public class Solution {
 
 
         return answer - 1;
+    */
+
+        return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+                .values()
+                .stream()
+                .collect(reducing(1L, (x, y) -> x * (y + 1))).intValue() - 1;
+
+
     }
 
 }
